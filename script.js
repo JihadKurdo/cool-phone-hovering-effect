95163bff7fa17e3699c216004a95c6b26e039d3c
Jihad Kurdo <jihad.a.muhammad@gmail.com>
2023-06-02 02:25:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function time() {
 
-    let Number = Intl.NumberFormat("en-US", {
+    let number = Intl.NumberFormat("en-US", {
         minimumIntegerDigits: 2
     })
 
     let date = new Date()
 
-    document.querySelector(".phone span").textContent = Number.format(date.getHours()) + ":" + Number.format(date.getMinutes()) + ":" + Number.format(date.getSeconds())
+    document.querySelector(".phone span").textContent = number.format(date.getHours()) + ":" + number.format(date.getMinutes()) + ":" + number.format(date.getSeconds())
 
     window.requestAnimationFrame(time)
 
